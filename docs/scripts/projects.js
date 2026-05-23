d1b96eee3d848c0de975d0248577be56aa692089
Mohammad Taher Ahmadi <mohammad.1387.kavian@gmail.com>
2026-05-24 01:44:04 +0330

    changed projects path

--- docs/scripts/projects.js
+++ docs/scripts/projects.js
@@ -181,7 +181,7 @@ function renderProjects() {
 
 async function initProjects() {
   try {
-    const res = await fetch("./projects/projects.json");
+    const res = await fetch("projects/projects.json");
 
     allProjects = await res.json();
 
